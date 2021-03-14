a03b9333d55bf97f847081c5e8c5f7f06b0ae9f1
salih <salihsert18@gmail.com>
2021-03-14 22:46:16 +0100

    modify structure

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -12,7 +12,6 @@ const User = require("../../models/User");
 // @desc     Test route
 // @access   Private
 router.get("/", auth, async (req, res) => {
-
   try {
     const user = await User.findById(req.user.id).select("-password");
     res.json(user);
