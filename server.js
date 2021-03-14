782b3c23dbb5dd01b1b15d7ddab72bfb2bdfdbe0
salih <salihsert18@gmail.com>
2021-03-14 22:00:23 +0100

    modify server file

--- server.js
+++ server.js
@@ -17,16 +17,23 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/todos", require("./routes/api/todos"));
 app.use("/api/tags", require("./routes/api/tags"));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
 
-  app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+// // Serve static assets in production
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+
+//   app.use(express.static("client/build"));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 
