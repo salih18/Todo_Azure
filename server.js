71a739c332eee47812bbb98e3ab2ac61197a7897
salih <salihsert18@gmail.com>
2021-03-14 23:46:31 +0100

    update

--- server.js
+++ server.js
@@ -18,22 +18,22 @@ app.use("/api/todos", require("./routes/api/todos"));
 app.use("/api/tags", require("./routes/api/tags"));
 
 
-app.use(express.static("client/build"));
+// app.use(express.static("client/build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
 
-// // Serve static assets in production
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
 
-//   app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
